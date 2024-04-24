0444a7958fa5d63dcb90a45024a4201244e9addd
Justin Rivest <justin@smarty.com>
2024-04-24 17:32:42 -0600

    Added Ability Score related Power functionality. Also cleaned up getData() function. Added Beast to roster

--- src/components/character-sheet/AbilityScores.js
+++ src/components/character-sheet/AbilityScores.js
@@ -12,44 +12,44 @@ export default function AbilityScores() {
 
                 <div className="title">Ability Scores</div>
                 <div className="ability-score">
-                    <div><b>Melee</b>: {character.melee}</div>
+                    <div><b className="ability-title">Melee</b>: {character.melee} {character.meleeEdge}</div>
                     <div>Melee Defense: {character.mDefense}</div>
-                    <div>Non-Combat Modifier: +{character.mNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.mNonCombat} {character.meleeEdge}</div>
                     <br />
                 </div>
 
                 <div className="ability-score">
-                    <div><b className="ability-title">Agility</b>: {character.agility}</div>
+                    <div><b className="ability-title">Agility</b>: {character.agility} {character.agilityEdge}</div>
                     <div>Agility Defense: {character.aDefense}</div>
-                    <div>Non-Combat Modifier: +{character.aNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.aNonCombat} {character.agilityEdge}</div>
                     <br />
                 </div>
                 
                 <div className="ability-score">
-                    <div><b className="ability-title">Resilience</b>: {character.resilience}</div>
+                    <div><b className="ability-title">Resilience</b>: {character.resilience} {character.resilienceEdge}</div>
                     <div>Resilience Defense: {character.rDefense}</div>
-                    <div>Non-Combat Modifier: +{character.rNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.rNonCombat} {character.resilienceEdge}</div>
                     <br />
                 </div>
 
                 <div className="ability-score">
-                    <div><b className="ability-title">Vigilance</b>: {character.vigilance}</div>
+                    <div><b className="ability-title">Vigilance</b>: {character.vigilance} {character.vigilanceEdge}</div>
                     <div>Vigilance Defense: {character.vDefense}</div>
-                    <div>Non-Combat Modifier: +{character.vNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.vNonCombat} {character.vigilanceEdge}</div>
                     <br />
                 </div>
 
                 <div className="ability-score">
-                    <div><b className="ability-title">Ego</b>: {character.ego}</div>
+                    <div><b className="ability-title">Ego</b>: {character.ego} {character.egoEdge}</div>
                     <div>Ego Defense: {character.eDefense}</div>
-                    <div>Non-Combat Modifier: +{character.eNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.eNonCombat} {character.egoEdge}</div>
                     <br />
                 </div>
 
                 <div className="ability-score">
-                    <div><b className="ability-title">Logic</b>: {character.logic}</div>
+                    <div><b className="ability-title">Logic</b>: {character.logic} {character.logicEdge}</div>
                     <div>Logic Defense: {character.lDefense}</div>
-                    <div>Non-Combat Modifier: +{character.lNonCombat}</div>
+                    <div>Non-Combat Modifier: +{character.lNonCombat} {character.logicEdge}</div>
                 </div>
             </div>
         </section>
--- src/objectClasses/character.js
+++ src/objectClasses/character.js
@@ -10,18 +10,21 @@ export default class Character {
         this.name = characterData.name;
         this.image = characterData.image;
         this.rank = characterData.rank;
+        
         this.melee = characterData.melee;
         this.agility = characterData.agility;
         this.resilience = characterData.resilience;
         this.vigilance = characterData.vigilance;
         this.ego = characterData.ego;
         this.logic = characterData.logic;
+        
         this.powerSets = characterData.hasClassObjects ? characterData.powers : Object.entries(characterData.powers).map((powerSet) => {
             const [name, powers] = powerSet;
             return new PowerSet(name, powers)
         })
         this.traits = characterData.hasClassObjects ? characterData.traits : characterData.traits.map((trait) => {return new Trait(trait)})
         this.tags = characterData.hasClassObjects ? characterData.tags : characterData.tags.map((tag) => {return new Tag(tag)})
+        
         this.biography = characterData.biography;
         this.occupation = characterData.biography.occupation.map((occupation) => {return new Occupation(occupation)})
         this.origin = characterData.biography.origin.map((origin) => {return new Origin(origin)})
@@ -128,6 +131,13 @@ export default class Character {
         this.eNonCombat = characterData.eNonCombat;
         this.lNonCombat = characterData.lNonCombat;
 
+        this.meleeEdge = characterData.meleeEdge;
+        this.agilityEdge = characterData.agilityEdge;
+        this.resilienceEdge = characterData.resilienceEdge;
+        this.vigilanceEdge = characterData.vigilanceEdge;
+        this.egoEdge = characterData.egoEdge;
+        this.logicEdge = characterData.logicEdge;
+
     }
     else {
         this.mDefense = this.melee + 10;
@@ -143,6 +153,13 @@ export default class Character {
         this.vNonCombat = this.vigilance;
         this.eNonCombat = this.ego;
         this.lNonCombat = this.logic;
+
+        this.meleeEdge = "";
+        this.agilityEdge = "";
+        this.resilienceEdge = "";
+        this.vigilanceEdge = "";
+        this.egoEdge = "";
+        this.logicEdge = "";
 
         //Check for powers/traits/tags that may effect these scores.
     }
@@ -204,7 +221,7 @@ export default class Character {
     const traitsThatChangeStats = {
         "Battle Ready": () => {this.maxFocus += 30; this.resetFocus()},
         "Situational Awareness": () => this.initModifierEdge = "E",
-        "Big (Reach 2)": () => this.runSpeed++,
+        "Big": () => {this.runSpeed++; this.mDefense--; this.aDefense--}, // Big should also extend Reach to 2
     }
     this.traits.forEach((trait) => {
         if (trait.name in traitsThatChangeStats) {
@@ -216,7 +233,10 @@ export default class Character {
 
    updatePowerStats() {
     const powersThatChangeStats = {
-        "Spider-Sense": () => this.initModifierEdge = "E",
+        //Comments in this object are additional features that still need to be implemented.
+        "Spider-Sense": () => {this.initModifierEdge = "E"; this.aDefense += 2},
+        "Additional Limb": () => {this.meleeEdge = "E"; this.agilityEdge = "E"},
+
         "Jump 1": () => {this.jumpSpeed = this.runSpeed},
         "Jump 2": () => {this.jumpSpeed = this.rank * this.runSpeed; this.nonCombatJumpSpeed = this.jumpSpeed * 3},
         "Jump 3": () => {this.jumpSpeed = this.rank * this.runSpeed; this.nonCombatJumpSpeed = this.jumpSpeed * 50},
@@ -227,6 +247,26 @@ export default class Character {
         "Webgliding": () => {this.glideSpeed = this.runSpeed * 2},
         "Levitation": () => {this.levitateSpeed = this.runSpeed},
 
+        "Mighty 1": () => {this.addMeleeNonCombat(1)}, // +1 to Melee Damage Multiplier. Treat as 1 size bigger for throwing/lifting/carrying/swinging.
+        "Mighty 2": () => {this.addMeleeNonCombat(2)}, // +2 to Melee Damage Multiplier. Treat as 2 sizes bigger for throwing/lifting/carrying/swinging.
+        "Mighty 3": () => {this.addMeleeNonCombat(3)}, // +3 to Melee Damage Multiplier. Treat as 3 sizes bigger for throwing/lifting/carrying/swinging.
+        "Mighty 4": () => {this.addMeleeNonCombat(4)}, // +4 to Melee Damage Multiplier. Treat as 4 sizes bigger for throwing/lifting/carrying/swinging.
+        "Accuracy 1": () => {this.addAgiiltyNonCombat(1)}, // +1 to Agility Damage Multiplier. 
+        "Accuracy 2": () => {this.addAgiiltyNonCombat(2)}, // +2 to Agility Damage Multiplier.
+        "Accuracy 3": () => {this.addAgiiltyNonCombat(3)}, // +3 to Agility Damage Multiplier.
+        "Accuracy 4": () => {this.addAgiiltyNonCombat(4)}, // +4 to Agility Damage Multiplier.
+        "Discipline 1": () => {this.addEgoNonCombat(1)}, // +1 to Ego Damage Multiplier.
+        "Discipline 2": () => {this.addEgoNonCombat(2)}, // +2 to Ego Damage Multiplier.
+        "Discipline 3": () => {this.addEgoNonCombat(3)}, // +3 to Ego Damage Multiplier.
+        "Discipline 4": () => {this.addEgoNonCombat(4)}, // +4 to Ego Damage Multiplier.
+        "Brilliance 1": () => {this.addLogicNonCombat(1)}, // +1 to Logic Damage Multiplier.
+        "Brilliance 2": () => {this.addLogicNonCombat(2)}, // +2 to Logic Damage Multiplier.
+        "Brilliance 3": () => {this.addLogicNonCombat(3)}, // +3 to Logic Damage Multiplier.
+        "Brilliance 4": () => {this.addLogicNonCombat(4)}, // +4 to Logic Damage Multiplier.
+
+        "Evasion": () => {this.hasEvasion = true;},
+        "Integrity": () => {this.hasIntegrity = true;},
+        "Brawling": () => {this.hasBrawling = true;}
     }
     this.powerSets.forEach(powerSet => powerSet.powers.forEach((power) => {
         if (power.name in powersThatChangeStats) {
@@ -234,29 +274,35 @@ export default class Character {
             func()
         }
     }))
+    
+    // If the character has these powers, execute functionality at the last second.
+    if (this.hasEvasion && this.aDefense > this.mDefense) this.mDefense = this.aDefense;
+    if (this.hasIntegrity && this.lDefense > this.eDefense) this.eDefense = this.lDefense;
+    if (this.hasBrawling && this.mDefense > this.aDefense) this.aDefense = this.mDefense;
    }
+
+   addMeleeNonCombat(value) { this.mNonCombat += value}
+   addAgiiltyNonCombat(value) { this.aNonCombat += value}
+   addResilienceNonCombat(value) { this.rNonCombat += value}
+   addVigilanceNonCombat(value) { this.vNonCombat += value}
+   addEgoNonCombat(value) { this.eNonCombat += value}
+   addLogicNonCombat(value) { this.lNonCombat += value}
+
 
    getData() {
     return {
         id: this.id,
         name: this.name,
         rank: this.rank,
+        image: this.image,
+
         melee: this.melee,
         agility: this.agility,
         resilience: this.resilience,
         vigilance: this.vigilance,
         ego: this.ego,
         logic: this.logic,
-        image: this.image,
-        powers: this.powerSets,
-        traits: this.traits,
-        tags: this.tags,
-        biography: this.biography,
-        maxHealth: this.maxHealth,
-        health: this.health,
-        maxFocus: this.maxFocus,
-        focus: this.focus,
-        karma: this.karma,
+        
         mDefense: this.mDefense,
         aDefense: this.aDefense,
         rDefense: this.rDefense,
@@ -269,10 +315,29 @@ export default class Character {
         vNonCombat: this.vNonCombat,
         eNonCombat: this.eNonCombat,
         lNonCombat: this.lNonCombat,
+        meleeEdge: this.meleeEdge,
+        agilityEdge: this.agilityEdge,
+        resilienceEdge: this.resilienceEdge,
+        vigilanceEdge: this.vigilanceEdge,
+        egoEdge: this.egoEdge,
+        logicEdge: this.logicEdge,
+
+        powers: this.powerSets,
+        traits: this.traits,
+        tags: this.tags,
+        biography: this.biography,
+        
+        maxHealth: this.maxHealth,
+        health: this.health,
+        maxFocus: this.maxFocus,
+        focus: this.focus,
+        karma: this.karma,
+        
         healthDamageReduction: this.healthDamageReduction,
         focusDamageReduction: this.focusDamageReduction,
         initModifier: this.initModifier,
         initModifierEdge: this.initModifierEdge,
+        
         runSpeed: this.runSpeed,
         climbSpeed: this.climbSpeed,
         jumpSpeed: this.jumpSpeed,
@@ -283,8 +348,9 @@ export default class Character {
         swingSpeed: this.swingSpeed,
         glideSpeed: this.glideSpeed,
         levitateSpeed: this.levitateSpeed,
-        hasClassObjects: true
 
+        
+        hasClassObjects: true
     }
    }
 
